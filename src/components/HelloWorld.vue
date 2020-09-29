6041147a89b6c0888c389e85b8c7d5025ad520bc
sambit sahoo <soulsam480@hotmail.com>
2020-09-30 01:34:06 +0530

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <label for="email">Email</label>
+  <input type="email" name="email" id="email" v-model="user.email" />
+  <br />
+  <br />
+  <label for="passd">Password</label>
+  <input
+    type="password"
+    name="password"
+    id="passd"
+    v-model="user.password"
+  /><br />
+  <br />
+  <button @click="login">Login</button>
+
+  <br />
+  <br />
+
+  <button @click="newToken">Request for new token</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, reactive, ref } from "vue";
+import axios from "axios";
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  setup() {
+    const user = reactive({
+      email: "" as string,
+      password: "" as string,
+    });
+    const login = () => {
+      axios({
+        method: "post",
+        url: "http://localhost:4000/login",
+        data: {
+          email: user.email,
+          password: user.password,
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    };
+
+    const newToken = () => {
+      axios({
+        method: "post",
+        url: "http://localhost:4000/token",
+        withCredentials: true,
+        xsrfCookieName: 'refreshToken',
+        
+      }).then((res) => {
+        console.log(res);
+      });
+    };
+    return { user, login, newToken };
   },
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style lang="scss" scoped></style>
